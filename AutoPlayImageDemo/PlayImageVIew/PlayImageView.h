054bc210160e714d19266c525f7ebefd655ec229
Massimo <16707587@qq.com>
2015-10-30 12:01:51 +0800

    no message

---
+++ AutoPlayImageDemo/PlayImageVIew/PlayImageView.h
@@ -0,0 +1,15 @@
+//
+//  PlayImageView.h
+//  AutoPlayImageDemo
+//
+//  Created by Massimo on 15/10/29.
+//  Copyright © 2015年 Massimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayImageView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame views:(NSArray*)views;
+
+@end
